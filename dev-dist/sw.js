a74e2b60a7e2368f511fde2318b9b161bbbd3d96
Ángel Caparrós Caballero <135209076+kubaliski@users.noreply.github.com>
2025-05-05 19:51:23 +0200

    refactor: cambio imagen más grande

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -86,7 +86,7 @@ define(['./workbox-523f56e0'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.d4qiub4681"
+    "revision": "0.0600odtvd5g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

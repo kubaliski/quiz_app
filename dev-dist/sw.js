f7ef05723b28bb7e5dc1157b2073c0de9c732d33
Ángel Caparrós Caballero <135209076+kubaliski@users.noreply.github.com>
2025-05-05 11:18:28 +0200

    fix: cambio importaciones modulos para prod

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -86,7 +86,7 @@ define(['./workbox-523f56e0'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qlbb205957g"
+    "revision": "0.d4qiub4681"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

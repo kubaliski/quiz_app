0b5c7a0f655213fe7ff4f562e8ab7896b7da95ea
Camilo ~ <camilochzzb@gmail.com>
2025-05-14 14:25:27 +0200

    Feat: Anadir modulo Simulacro de Bloque 8 de Programacion.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -86,7 +86,7 @@ define(['./workbox-84006cf1'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ab29hcp2n7o"
+    "revision": "0.ojvn5n80bko"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

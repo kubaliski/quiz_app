cd2d0c84b13dec68fd76de863bebd5262330bd8f
Ángel Caparrós Caballero <135209076+kubaliski@users.noreply.github.com>
2025-05-18 20:29:06 +0200

    feat: anadir examen 18/05 bd

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -86,7 +86,7 @@ define(['./workbox-84006cf1'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.nnrdsr2hhr8"
+    "revision": "0.fhjrqorahso"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

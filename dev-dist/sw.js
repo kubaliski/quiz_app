cf1619631f3a24022aad05435029ea64e7d2692d
kubaliski <angel.ccapb@gmail.com>
2026-01-28 12:06:53 +0100

    feat:add_new_test

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -86,7 +86,7 @@ define(['./workbox-027ecc3a'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rrlnlllosbo"
+    "revision": "0.egah5f2a2b8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

0c25790120a32994847ab6e815e352c0ff2fe3ca
kubaliski <angel.ccapb@gmail.com>
2026-04-23 15:48:50 +0200

    feature/ add sim 1 for mpo2 and sos

--- src/data/asignaturas/moduloProfesionalOptativo2/index.js
+++ src/data/asignaturas/moduloProfesionalOptativo2/index.js
@@ -1,22 +1,25 @@
-import info from './info.js';
-import moduloUnidadUnoCloudComputing from './modulos/unidad1.js';
-import moduloUnidadDosInfraestructuraGlobal from './modulos/unidad2.js';
-import moduloUnidadTresSeguridadAWS from './modulos/unidad3.js';
-import moduloUnidadCuatroRedesEnLaNube from './modulos/unidad4.js';
-import moduloUnidadCincoServiciosDeComputo from './modulos/unidad5.js';
-import moduloExamenUno from './modulos/preguntasExamen1.js';
-import moduloExamenDos from './modulos/preguntasExamen2.js';
+import info from "./info.js";
+import moduloUnidadUnoCloudComputing from "./modulos/unidad1.js";
+import moduloUnidadDosInfraestructuraGlobal from "./modulos/unidad2.js";
+import moduloUnidadTresSeguridadAWS from "./modulos/unidad3.js";
+import moduloUnidadCuatroRedesEnLaNube from "./modulos/unidad4.js";
+import moduloUnidadCincoServiciosDeComputo from "./modulos/unidad5.js";
+import moduloExamenUno from "./modulos/preguntasExamen1.js";
+import moduloExamenDos from "./modulos/preguntasExamen2.js";
+import simulacroUnoModuloProfesional1 from "./modulos/simulacro1.js";
 
 export const moduloProfesionalOptativo = {
-    ...info,
-    modulos: [
-        moduloUnidadUnoCloudComputing,
-        moduloUnidadDosInfraestructuraGlobal,
-        moduloUnidadTresSeguridadAWS,
-        moduloUnidadCuatroRedesEnLaNube,
-        moduloUnidadCincoServiciosDeComputo,
-        moduloExamenUno,
-        moduloExamenDos
-    ],
+  ...info,
+  modulos: [
+    moduloUnidadUnoCloudComputing,
+    moduloUnidadDosInfraestructuraGlobal,
+    moduloUnidadTresSeguridadAWS,
+    moduloUnidadCuatroRedesEnLaNube,
+    moduloUnidadCincoServiciosDeComputo,
+    moduloExamenUno,
+    moduloExamenDos,
+    simulacroUnoModuloProfesional1,
+  ],
 };
 export default moduloProfesionalOptativo;
+
--- src/data/asignaturas/sostenibilidad/index.js
+++ src/data/asignaturas/sostenibilidad/index.js
@@ -1,19 +1,21 @@
-import info from './info.js';
-import moduloUnidadDosCambioClimaticoDescarbonizacion from './modulos/unidadDos.js';
-import moduloUnidadTresEconomiaCircularAplicacion from './modulos/unidadTres.js';
-import moduloUnidadCuatroConservacionEcosistemas from './modulos/unidadCuatro.js';
-import moduloUnidadCincoEquidadBienestarSocial from './modulos/unidadCinco.js';
-import moduloTestUnoSostenibilidad from './modulos/testUno.js';
+import info from "./info.js";
+import moduloUnidadDosCambioClimaticoDescarbonizacion from "./modulos/unidadDos.js";
+import moduloUnidadTresEconomiaCircularAplicacion from "./modulos/unidadTres.js";
+import moduloUnidadCuatroConservacionEcosistemas from "./modulos/unidadCuatro.js";
+import moduloUnidadCincoEquidadBienestarSocial from "./modulos/unidadCinco.js";
+import moduloTestUnoSostenibilidad from "./modulos/testUno.js";
+import simulacroUnoSostenibilidad from "./modulos/simulacro1.js";
 
 export const sostenibilidad = {
-    ...info,
-    modulos: [
-        moduloUnidadDosCambioClimaticoDescarbonizacion,
-        moduloUnidadTresEconomiaCircularAplicacion,
-        moduloUnidadCuatroConservacionEcosistemas,
-        moduloUnidadCincoEquidadBienestarSocial,
-        moduloTestUnoSostenibilidad
-    ]
-
+  ...info,
+  modulos: [
+    moduloUnidadDosCambioClimaticoDescarbonizacion,
+    moduloUnidadTresEconomiaCircularAplicacion,
+    moduloUnidadCuatroConservacionEcosistemas,
+    moduloUnidadCincoEquidadBienestarSocial,
+    moduloTestUnoSostenibilidad,
+    simulacroUnoSostenibilidad,
+  ],
 };
 export default sostenibilidad;
+

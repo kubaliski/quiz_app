b1dabf947ee0d69ca9092a2a95b1b10f4719edfd
kubaliski <angel.ccapb@gmail.com>
2026-05-12 20:19:32 +0200

    feature: add dig & deploy sims

--- src/data/asignaturas/digitalizacion/index.js
+++ src/data/asignaturas/digitalizacion/index.js
@@ -1,22 +1,25 @@
-import info from './info.js';
-import moduloUnidadDosTecnologiasHabilitadorasDigitales from './modulos/unidadDos.js';
-import moduloUnidadTresSistemasConectados from './modulos/unidadTres.js';
-import moduloUnidadCuatroIAyBigData from './modulos/unidadCuatro.js';
-import moduloUnidadCincoValorDelDato from './modulos/unidadCinco.js';
-import moduloUnidadSeisTransformacionDigital from './modulos/unidadSeis.js';
-import moduloTestUnoDigitalizacion from './modulos/testUno.js';
-import moduloTestDosDigitalizacion from './modulos/testDos.js';
+import info from "./info.js";
+import moduloUnidadDosTecnologiasHabilitadorasDigitales from "./modulos/unidadDos.js";
+import moduloUnidadTresSistemasConectados from "./modulos/unidadTres.js";
+import moduloUnidadCuatroIAyBigData from "./modulos/unidadCuatro.js";
+import moduloUnidadCincoValorDelDato from "./modulos/unidadCinco.js";
+import moduloUnidadSeisTransformacionDigital from "./modulos/unidadSeis.js";
+import moduloTestUnoDigitalizacion from "./modulos/testUno.js";
+import moduloTestDosDigitalizacion from "./modulos/testDos.js";
+import moduloSimulacroUnoDigitalizacion from "./modulos/simulacro1.js";
 
 export const digitalizacion = {
-    ...info,
-    modulos: [
-        moduloUnidadDosTecnologiasHabilitadorasDigitales,
-        moduloUnidadTresSistemasConectados,
-        moduloUnidadCuatroIAyBigData,   
-        moduloUnidadCincoValorDelDato,
-        moduloUnidadSeisTransformacionDigital,
-        moduloTestUnoDigitalizacion,
-        moduloTestDosDigitalizacion
-    ]
-}; 
+  ...info,
+  modulos: [
+    moduloUnidadDosTecnologiasHabilitadorasDigitales,
+    moduloUnidadTresSistemasConectados,
+    moduloUnidadCuatroIAyBigData,
+    moduloUnidadCincoValorDelDato,
+    moduloUnidadSeisTransformacionDigital,
+    moduloTestUnoDigitalizacion,
+    moduloTestDosDigitalizacion,
+    moduloSimulacroUnoDigitalizacion,
+  ],
+};
 export default digitalizacion;
+
